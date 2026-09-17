5b6b560b12492339caeaa5df3ab25ecbd806d328
Nicolaas Stewart (1000150639) <STEWNJ4@student.op.ac.nz>
2026-09-17 16:19:57 +1200

    Hi

--- GuessingGame/GuessingGame/Program.cs
+++ GuessingGame/GuessingGame/Program.cs
@@ -48,7 +48,7 @@
             } while (userGuess != guessTarget);
             Thread.Sleep(800);
             Console.WriteLine("Congratulations, you guessed the right number!");
-            Console.WriteLine("WOOOOWWW!");
+            Console.WriteLine("testing...");
             Console.ReadLine();
         }
     }
